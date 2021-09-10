17a94fb8c9374b60a5fd3dc41131d67c970a7e35
Maryust <masyanya1101@yandex.ru>
2021-09-10 13:00:50 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,12 @@ public class Main {
         // Внесённая сумма
         int endSumInAccount;
         int bonus;
-        if (sumDeposit>1000){
-            bonus = sumDeposit/100;
+        if (sumDeposit > 1000) {
+            bonus = sumDeposit / 100;
             endSumInAccount = startSumInAccount + sumDeposit + bonus;
             System.out.println("Баланс на счете:" + endSumInAccount);
             System.out.println("Получен бонус:" + bonus);
-        }
-        else {
+        } else {
             endSumInAccount = startSumInAccount + sumDeposit;
             System.out.println("Баланс на счете:" + endSumInAccount);
             System.out.println("Пополните баланс больше чем на 1000 рублей, чтобы получить бонус");
